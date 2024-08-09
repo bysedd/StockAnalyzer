2661d0d8792060102c64a8003fc279556d97b15c
Felippe Andrade <4drade@gmail.com>
2024-08-09 16:02:47 -0300

    refactor: modularize main functionalities into util module
    - Moved data collection, ticker validation, and email sending functions to util module.
    - Removed redundant imports and streamlined environment variable checks.

--- main.py
+++ main.py
@@ -1,51 +1,27 @@
 import os
-import smtplib
 from datetime import datetime
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from typing import Dict
 
 from dotenv import load_dotenv
-from yfinance import Ticker
+
+import util
 
 # Carregar variáveis de ambiente e definir credenciais
 load_dotenv()
 my_email = os.environ["MY_EMAIL"]
 password = os.environ["PASSWORD"]
+login = (my_email, password)
 
-if not my_email or not password:
+if not all(login):
     print("Email or password not found in environment variables!")
     exit(1)
 
-
-def collect_data(ticker: str, start: str, end: str) -> Dict[str, float]:
-    """
-    Função para coletar dados de fechamento de uma ação
-
-    :param ticker: Código da ação
-    :param start: Período de início
-    :param end: Período de fim
-    :return: Dicionário com os valores de fechamento
-    """
-    try:
-        df = Ticker(ticker).history(start=start, end=end)
-        close = df["Close"]
-        if close.empty:
-            raise ValueError("Dados de fechamento não encontrados")
-        return {
-            "max": round(close.max(), 2),
-            "min": round(close.min(), 2),
-            "medium": round(close.mean(), 2),
-        }
-    except Exception as e:
-        print(f"Ocorreu um erro ao coletar os dados: {e}")
-        return {}
-
-
 ticker = input("Digite o código da ação desejada: ")  # example: BBAS3.SA
+
+if not util.validate_ticker(ticker):
+    exit(1)
+
 start = input("Digite a data de início (yyyy-mm-dd): ")
 end = input("Digite a data de fim (yyyy-mm-dd): ")
-
 try:
     datetime.strptime(start, "%Y-%m-%d")
     datetime.strptime(end, "%Y-%m-%d")
@@ -53,36 +29,7 @@ except ValueError:
     print("Formato de data inválido. Use (yyyy-mm-dd)")
     exit(1)
 
-closure = collect_data(ticker, start, end)
-
-
-def send_email(subject: str, message: str, to: str) -> bool:
-    """
-    Função para enviar email usando o Gmail como servidor SMTP
-
-    :param subject: Assunto, título do email
-    :param message: Mensagem, corpo do email
-    :param to: Destinatário do email
-    :return: True se o email foi enviado com sucesso, False caso contrário
-    """
-    try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
-        server.login(my_email, password)
-
-        msg = MIMEMultipart()
-        msg["From"] = my_email
-        msg["To"] = to
-        msg["Subject"] = subject
-        msg.attach(MIMEText(message, "plain"))
-
-        server.sendmail(my_email, to, msg.as_string())
-        server.quit()
-        return True
-    except Exception as e:
-        print(f"Erro ao enviar e-mail: {e}")
-        return False
-
+closure = util.collect_data(ticker, start, end)
 
 subject = "Relatório de Análise Financeira"
 message = f"""
@@ -100,5 +47,5 @@ Atenciosamente,
 [Seu Nome]
 """
 
-if send_email(subject, message, my_email):
-    print("E-mail enviado com sucesso!")
+# if util.send_email(login, subject, message, to=my_email):
+#     print("E-mail enviado com sucesso!")
