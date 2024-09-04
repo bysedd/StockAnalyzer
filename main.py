cc4e106cf4977d8b3b36fb3d168a8141d47382c4
Felippe Andrade <4drade@gmail.com>
2024-09-04 14:08:25 -0300

    feat: add user name environment variable and refactor email reporting
    - Add NAME environment variable to store the user's name
    - Refactor main script to use a main function
    - Include the user's name in the email signature
    - Uncomment the email sending code

--- main.py
+++ main.py
@@ -5,47 +5,52 @@ from dotenv import load_dotenv
 
 import util
 
-# Carregar variáveis de ambiente e definir credenciais
-load_dotenv()
-my_email = os.environ["MY_EMAIL"]
-password = os.environ["PASSWORD"]
-login = (my_email, password)
 
-if not all(login):
-    print("Email or password not found in environment variables!")
-    exit(1)
+def main() -> None:
+    # Carregar variáveis de ambiente e definir credenciais
+    load_dotenv()
+    my_email = os.environ["MY_EMAIL"]
+    password = os.environ["PASSWORD"]
+    name = os.environ["NAME"]
+    login = (my_email, password)
 
-ticker = input("Digite o código da ação desejada: ")  # example: BBAS3.SA
+    if not all(login):
+        print("Email or password not found in environment variables!")
+        exit(1)
 
-if not util.validate_ticker(ticker):
-    exit(1)
+    ticker = input("Digite o código da ação desejada: ")  # example: BBAS3.SA
+    if not util.validate_ticker(ticker):
+        exit(1)
 
-start = input("Digite a data de início (yyyy-mm-dd): ")
-end = input("Digite a data de fim (yyyy-mm-dd): ")
-try:
-    datetime.strptime(start, "%Y-%m-%d")
-    datetime.strptime(end, "%Y-%m-%d")
-except ValueError:
-    print("Formato de data inválido. Use (yyyy-mm-dd)")
-    exit(1)
+    start = input("Digite a data de início (yyyy-mm-dd): ")
+    end = input("Digite a data de fim (yyyy-mm-dd): ")
+    try:
+        datetime.strptime(start, "%Y-%m-%d")
+        datetime.strptime(end, "%Y-%m-%d")
+    except ValueError:
+        print("Formato de data inválido. Use (yyyy-mm-dd)")
+        exit(1)
 
-closure = util.collect_data(ticker, start, end)
+    closure = util.collect_data(ticker, start, end)
+    subject = "Relatório de Análise Financeira"
+    message = f"""
+    Prezado,
 
-subject = "Relatório de Análise Financeira"
-message = f"""
-Prezado,
+    Segue abaixo um resumo das informações coletadas referentes ao ticker {ticker}:
 
-Segue abaixo um resumo das informações coletadas referentes ao ticker {ticker}:
+    - Cotação Máxima: R$ {closure["max"]}
+    - Cotação Mínima: R$ {closure["min"]}
+    - Valor Médio: R$ {closure["medium"]}
 
-- Cotação Máxima: R$ {closure["max"]}
-- Cotação Mínima: R$ {closure["min"]}
-- Valor Médio: R$ {closure["medium"]}
+    Agradeço a sua atenção.
 
-Agradeço a sua atenção.
+    Atenciosamente,
+    {name}
+    """
 
-Atenciosamente,
-[Seu Nome]
-"""
+    if util.send_email(login, subject, message, to=my_email):
+        print("E-mail enviado com sucesso!")
 
-# if util.send_email(login, subject, message, to=my_email):
-#     print("E-mail enviado com sucesso!")
+
+if __name__ == "__main__":
+    main()
